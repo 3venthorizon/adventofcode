7b2105f14c04a97d5493f4decfdc1062104bc760
Dewald Pretorius <pretorius.dewald@gmail.com>
2022-12-12 06:58:29 +0200

    Wrong answer Day 8

--- src/main/java/com/adventofcode/Day8.java
+++ src/main/java/com/adventofcode/Day8.java
@@ -7,12 +7,12 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 
 import com.opencsv.stream.reader.LineReader;
 
 public class Day8 {
+   
+   static final byte TREE_TALLEST = '9';
 
    BufferedReader createReader() throws IOException, URISyntaxException {
       ClassLoader classLoader = getClass().getClassLoader();
@@ -22,27 +22,76 @@ public class Day8 {
    }
    
    public long part1() throws IOException, URISyntaxException {
-      List<byte[]> forrest = loadForrest();
-      long north;
+      byte[] forrest = growForrest();
+      int length = (int) Math.sqrt(forrest.length);
+      long count = (length - 1L) * 4L; //perimeter visible
+      
+      for (int rowIndex = 1, rowLimit = length - 1; rowIndex < rowLimit; rowIndex++) {
+         byte ltrMin = forrest[rowIndex * length];
+         byte rtlMin = forrest[rowIndex * length + length - 1];
+         
+         for (int colIndex = 1, colLimit = length - 1; colIndex < colLimit; colIndex++) {
+            byte ltr = forrest[rowIndex * length + colIndex];
+            byte rtl = forrest[rowIndex * length + length - colIndex - 1];
+            
+            if (ltr > ltrMin) {
+               ltrMin = ltr;
+               count++;
+            }
+            
+            if (rtl > rtlMin) {
+               rtlMin = rtl;
+               count++;
+            }
+            
+            if (ltr == TREE_TALLEST && rtl == TREE_TALLEST) break;
+         }
+      }
+      
+      for (int colIndex = 1, colLimit = length - 1; colIndex < colLimit; colIndex++) {
+         byte ttbMin = forrest[colIndex];
+         byte bttMin = forrest[length * (length - 1) + colIndex];
+
+         for (int rowIndex = 1, rowLimit = length - 1; rowIndex < rowLimit; rowIndex++) {
+            byte ttb = forrest[rowIndex * length + colIndex];
+            byte btt = forrest[rowIndex * (length - 1) - colIndex];
+            
+            if (ttb > ttbMin) {
+               ttbMin = ttb;
+               count++;
+            }
+            
+            if (btt > bttMin) {
+               bttMin = btt;
+               count++;
+            }
+            
+            if (ttb == TREE_TALLEST && btt == TREE_TALLEST) break;
+         }
+      }
       
       
-      
-      return 0L;
+      return count;
    }
    
-   List<byte[]> loadForrest() throws IOException, URISyntaxException {
-      List<byte[]> forrest = new ArrayList<>(); 
+   byte[] growForrest() throws IOException, URISyntaxException {
+      StringBuilder sb = new StringBuilder(99 * 99);
+      int count = 0;
       
       try (BufferedReader reader = createReader()) {
          LineReader lineReader = new LineReader(reader, false);
          String line = lineReader.readLine();
          
          while (line != null) {
-            forrest.add(line.getBytes());
+            count++;
+            sb.append(line);
             line = lineReader.readLine();
          }
       }
       
-      return forrest;
+      byte[] trees = sb.toString().getBytes();
+      
+      if (trees.length / count != count) throw new RuntimeException("Not a square forrest");
+      return trees;
    }
 }
