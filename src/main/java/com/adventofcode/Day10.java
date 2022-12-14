ee97cb6d6251f177c64900273b50e90063ff9e83
Dewald Pretorius <pretorius.dewald@gmail.com>
2022-12-14 09:55:37 +0200

    Day 10

---
+++ src/main/java/com/adventofcode/Day10.java
@@ -0,0 +1,83 @@
+package com.adventofcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import com.opencsv.stream.reader.LineReader;
+
+public class Day10 {
+   static final String SPACE = " ";
+   static final String COMMAND_NOOP = "noop";
+   static final String COMMAND_ADDX = "addx ";
+   
+   static class State {
+      int cycles = 1;
+      int x = 1;
+      int nextCylce = 20;
+      int accumelator = 0;
+   }
+
+   BufferedReader createReader() throws IOException, URISyntaxException {
+      ClassLoader classLoader = getClass().getClassLoader();
+      URL resource = classLoader.getResource("day10.input");
+      Reader reader = Files.newBufferedReader(Paths.get(resource.toURI()));
+      return new BufferedReader(reader);
+   }
+   
+   public int part1() throws IOException, URISyntaxException {
+      State state = new State();
+      
+      try (BufferedReader reader = createReader()) {
+         LineReader lineReader = new LineReader(reader, false);
+         String line = lineReader.readLine();
+         
+         while (line != null) {
+            process(line, state);
+            line = lineReader.readLine();
+         }
+      }
+      
+      return state.accumelator;
+   }
+   
+   void process(String instruction, State state) {
+      if (COMMAND_NOOP.equals(instruction)) {
+         noop(state);
+         return;
+      }
+      if (!instruction.startsWith(COMMAND_ADDX)) return;
+      
+      int value = Integer.parseInt(instruction.substring(COMMAND_ADDX.length()));
+      addx(state, value);
+   }
+   
+   void noop(State state) {
+      accumulate(state);
+      state.cycles += 1;
+   }
+   
+   void addx(State state, int value) {
+      noop(state);
+      noop(state);
+      state.x += value;
+   }
+   
+   void accumulate(State state) {
+      crt(state);
+      if (state.cycles != state.nextCylce) return;
+      
+      state.accumelator += state.cycles * state.x;
+      state.nextCylce += 40;
+   }
+   
+   void crt(State state) {
+      char pixel = (Math.abs(((state.cycles - 1) % 40) - state.x) <= 1) ? '#' : '.';
+      System.out.print(pixel);
+      if (state.cycles % 40 == 0) System.out.println();
+   }
+}
---
+++ src/test/java/com/adventofcode/Day10Test.java
@@ -0,0 +1,19 @@
+package com.adventofcode;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.junit.jupiter.api.Test;
+
+class Day10Test {
+   
+   Day10 day10 = new Day10();
+
+   @Test
+   void testPart1() throws IOException, URISyntaxException {
+      System.out.println(day10.part1());
+   }
+
+}
