eb5d4691b5ca39e2b392c4575f9a478f6ae38137
Dewald Pretorius <pretorius.dewald@gmail.com>
2023-12-20 07:17:03 +0200

    Pattern matcher strategy

--- src/main/java/com/adventofcode/y2023/Day12.java
+++ src/main/java/com/adventofcode/y2023/Day12.java
@@ -1,41 +1,51 @@
 package com.adventofcode.y2023;
 
+import java.util.List;
+import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Day12 {
-   record Groups(String partial, int[] groupSizes) {}
 
-   record Arrangement(int[] numbers, int index) {}
+   record Arrangement(int[] numbers, int digit) {}
 
    public long part1(String fileName) {
-      return FileLineStreamer.read(fileName)
-            .map(this::extract)
-            .mapToLong(this::countArrangements)
-            .sum();
-   }
-
-   Groups extract(String line) {
-      String[] records = line.split(" ");
-      String[] sizes = records[1].split(",");
-      int[] groupSizes = Stream.of(sizes).mapToInt(Integer::parseInt).toArray();
-      return new Groups(records[0], groupSizes);
-   }
-
-   long countArrangements(Groups groups) {
-      int minLength = IntStream.of(groups.groupSizes()).sum() + groups.groupSizes.length - 1;
-      int radix = groups.partial.length() - minLength;
-      if (radix <= 0) return 1L;
-
-      int digits = groups.groupSizes.length + 1;
-      int[] numberSystem = new int[digits];
-
-      numberSystem[0] = radix;
-      numberSystem[groups.groupSizes.length] = 0;
-      IntStream.range(1, groups.groupSizes.length).forEach(index -> numberSystem[index] = 1);
-
 
       return 0L;
+   }
+
+   long countArrangements(String line) {
+      String[] records = line.split(" ");
+      String partial = records[0];
+      String[] sizes = records[1].split(",");
+      int[] groupSizes = Stream.of(sizes).mapToInt(Integer::parseInt).toArray();
+      int minLength = IntStream.of(groupSizes).sum() + groupSizes.length - 1;
+      int radix = partial.length() - minLength;
+      if (radix <= 0) return 1L;
+
+      int digits = groupSizes.length + 1;
+      int[] numbers = new int[digits];
+
+      numbers[0] = radix;
+      numbers[groupSizes.length] = 0;
+      IntStream.range(1, groupSizes.length).forEach(index -> numbers[index] = 1);
+
+      Arrangement arrangement = new Arrangement(numbers, digits);
+
+      return 0L;
+   }
+
+   Pattern compose(int[] groupSizes) {
+
+      return null;
+   }
+
+   boolean isValid(String partial, Arrangement arrangement, Pattern pattern) {
+      return false;
+   }
+
+   List<Arrangement> enumerate(Arrangement arrangement) {
+      return List.of();
    }
 
    public long part2() {
